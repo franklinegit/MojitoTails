7bdda9ec7216f0c4530225ced292139b318f6192
franklinegit <oddfrankline@gmail.com>
2025-06-24 08:02:12 +0300

    Add Navbar scrolltrigger to filter blur

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,15 +1,30 @@
 import React, {useEffect} from 'react';
 import { navLinks } from '../../Constants';
-// import gsap from 'gsap';
-// import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
-// gsap.registerPlugin(useGSAP);
+gsap.registerPlugin(useGSAP, ScrollTrigger);
 
 const Navbar = () => {
 
-	// useGSAP(() => {
+	useGSAP(() => {
+		const navTween = gsap.timeline({
+			scrollTrigger: {
+				trigger: 'nav',
+				start: 'bottom top'
+			}
+		});
 
-	// }, []);
+		navTween.fromTo('nav', {
+			backgroundColor: 'transparent'
+		}, {
+			backgroundColor: '#00000050',
+			backgroundFilter: 'blur(10px)',
+			duration: 1,
+			ease: 'power1.inOut'
+		});
+	}, []);
 	
 
 	return (
